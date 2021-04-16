e67df10808b5a1b2b62a58a8d12da3abb7e86620
Informat1on <arseniy.sherbak.2012@gmail.com>
2021-04-16 14:24:52 +0300

    added comments

--- modules/fkniga.py
+++ modules/fkniga.py
@@ -5,9 +5,13 @@ request = 'муму'
 source = requests.get(f'https://fkniga.ru/search/?q={request}&category=2721&page-size=2048').text
 soup  =  BeautifulSoup(source,'lxml')
 
+#получаю все карточки с книгами
 items = soup.find_all('div',class_='grid__item columnDesktop--3 columnSmallDesktop--4 columnTablet--4 columnMobile--12')
 
+# делаю перебор карточек
 for i in items:
+    # получаю название книги
     name = i .find('a',class_='card__title').text
+    # если в названии находится требуемый запрос
     if (name.startswith(request.capitalize()) or name.endswith(request.capitalize())):
         print(name)
