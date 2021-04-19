dbf38504b8adf5257c7f38c84d54e4e2abb33b05
Informat1on <arseniy.sherbak.2012@gmail.com>
2021-04-19 14:09:18 +0300

    Закончена работа над модулем mir-shkolnika

--- modules/mir-shkolnika.py
+++ modules/mir-shkolnika.py
@@ -2,9 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 
 def main(request):
-    source = requests.get(f'http://www.uchebnik.com/katalog/?filter%5Bsearch%5D={request}').text
+    cheap_book = {}
+    min_price = 9999999
+
+    source = requests.get(f'http://www.uchebnik.com/katalog/?items_per_page=100&filter%5Bsearch%5D={request}').text
     soup = BeautifulSoup(source,'lxml')
 
-    print(soup)
+    try:
+        # нахожу все элементы
+        items = soup.find_all('div',class_='tovar tile')
+        # делаю перебор каждого
+        for item in items:
+            # достаю цену
+            price = item.find_all('span',class_='item_price')
+            # если она со скидкой, то кладу в пееременную цену со скидкой
+            if (len(price) > 1):
+                price = int(price[1].text)
+            # если скидки нет
+            else:
+                price = int(price[0].text)
 
-main('муму')
+            # если цена меньше минимальной
+            if (price < min_price):
+                min_price = price
+                # достаю нужные данные
+                name = item.find('span', class_='name_tovar').text
+                link = 'http://www.uchebnik.com' + item.find('a').get('href')
+                image = 'http://www.uchebnik.com' + item.find('img').get('src')
+
+                # добавляю в словарь
+                cheap_book['name'] = name
+                cheap_book['price'] = price
+                cheap_book['link'] = link
+                cheap_book['image'] = image
+
+            else:
+                continue
+    # если случилось что то - печатаем ошибку
+    except Exception as e:
+        cheap_book['price'] = None
+        print(e)
+
+    return cheap_book
